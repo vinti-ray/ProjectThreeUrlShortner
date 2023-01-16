d0a1467d752c9d306153114e02a277c8f5552233
vinti-roy <vintiray71@gmail.com>
2023-01-16 16:58:54 +0530

    begin

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const app=express()
+const mongoose = require("mongoose");
+const route=require("./routes/route")
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose
+  .connect(
+    "mongodb+srv://vintiray:7091201680@cluster0.ahtxrqr.mongodb.net/groupXDatabase",
+    {
+      useNewUrlParser: true,
+    }
+  )
+
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+  app.use("/",route)
+
+  app.listen(3000,()=>{
+    console.log("Express app running on port " + 3000);
+  })
